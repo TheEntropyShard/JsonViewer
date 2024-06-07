c22cd66810fe7d7d3e60b74f141bb196ba4e9c64
TheEntropyShard <theentropyshard@gmail.com>
2024-06-07 19:31:58 +0300

    Added blank line

--- src/main/java/me/theentropyshard/jsonviewer/utils/Utils.java
+++ src/main/java/me/theentropyshard/jsonviewer/utils/Utils.java
@@ -38,11 +38,14 @@ public class Utils {
     }
 
     public static double round(double value, int places) {
-        if (places < 0) throw new IllegalArgumentException();
+        if (places < 0) {
+            throw new IllegalArgumentException();
+        }
 
         long factor = (long) Math.pow(10, places);
         value = value * factor;
         long tmp = Math.round(value);
+
         return (double) tmp / factor;
     }
 
