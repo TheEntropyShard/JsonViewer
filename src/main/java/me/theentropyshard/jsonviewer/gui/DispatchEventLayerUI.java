fb1b37562789a32308614397b8a428617f05acf6
TheEntropyShard <theentropyshard@gmail.com>
2025-01-10 23:30:26 +0300

    Updated copyright year

--- src/main/java/me/theentropyshard/jsonviewer/gui/DispatchEventLayerUI.java
+++ src/main/java/me/theentropyshard/jsonviewer/gui/DispatchEventLayerUI.java
@@ -1,6 +1,6 @@
 /*
  * JsonViewer - https://github.com/TheEntropyShard/JsonViewer
- * Copyright (C) 2023-2024 TheEntropyShard
+ * Copyright (C) 2023-2025 TheEntropyShard
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
